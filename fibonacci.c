479933b011b7c4b3294cf7afa947b7efff109e53
Dharanash Sundaramoorthi <Dharanash.moorthi@solitontech.local>
2023-08-03 11:49:54 +0530

    First Commit

---
+++ fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int* generateFibonacciSeries(int n){
+   int i;
+  int t1 = 0, t2 = 1;
+   int arr[n];
+  int nextTerm = t1 + t2;
+   arr[0]=t1;
+   arr[1]=t2;
+   arr[2]=nextTerm;
+  for (i = 3; i <= n; ++i) {
+    t1 = t2;
+    t2 = nextTerm;
+    nextTerm = t1 + t2;
+    arr[i]=nextTerm;
+  }
+   
+  return arr;
+}
+ 
+int main() {
+    int i=4;
+     int *arr1;
+    arr1=generateFibonacciSeries(i);
+     
+}
